de834a4bd8c318b8f99ecd426698229ad9d03633
ElieA99 <elie_aouad1999@hotmail.com>
2022-06-16 11:33:47 +0300

    Ready To Deploy

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Title = "Binary To Decimal";
+try
+{
+    for (int i = 0; ; i++)
+    {
+        Console.Write("Enter Binary Number: ");
+        string input = Console.ReadLine();
+        Console.Write("Decimal Number: ");
+        Console.Write("{0}", Convert.ToInt32(input, 2));
+        Console.WriteLine("\n");
+
+        Console.Write("Enter Decimal Number: ");
+        int inp = int.Parse(Console.ReadLine());
+        Console.Write("Binary Number: ");
+        Console.Write("{0}", Convert.ToString(inp, 2));
+        Console.WriteLine("\n");
+
+    }
+    
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
